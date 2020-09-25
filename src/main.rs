948e5f9c392563f7dcdffe35915980f5b28b4b15
Nbiba Bedis <bedisnbiba@gmail.com>
2020-09-25 12:00:02 +0100

    match awk behaviour on 0idx, fmt

--- src/main.rs
+++ src/main.rs
@@ -16,43 +16,53 @@ fn main() -> io::Result<()> {
         .parse()
         .expect("can not parse col number");
 
-	if col == 0 {
-		panic!("0 is an invalid col index");
-	}
+    if col == 0 {
+        write!(io::stdout(), "{}", buffer)?;
+        io::stdout().flush()?;
+        return Ok(());
+    }
 
-	let mut table: Vec<Vec<String>> = Default::default();
-	let mut tmp_row: Vec<String> = Default::default();
-	let mut tmp_str: String = Default::default();
+    let mut table: Vec<Vec<String>> = Default::default();
+    let mut tmp_row: Vec<String> = Default::default();
+    let mut tmp_str: String = Default::default();
 
-	let mut buffer = buffer.chars().peekable();
-	loop {
-		let c = buffer.next();
-		match c {
-			Some(' ') => {
-				tmp_row.push(tmp_str.drain(..).collect());
-				while buffer.peek()	== Some(&' ') {buffer.next();}
-			}
-			Some('\n') => {
-				tmp_row.push(tmp_str.drain(..).collect());
-				table.push(tmp_row.drain(..).collect());
-			}
-			Some(c) => {tmp_str.push(c);}
-			None => break
-		}
-	}
+    let mut buffer = buffer.chars().peekable();
+    loop {
+        let c = buffer.next();
+        match c {
+            Some(' ') => {
+                tmp_row.push(tmp_str.drain(..).collect());
+                while buffer.peek() == Some(&' ') {
+                    buffer.next();
+                }
+            }
+            Some('\n') => {
+                tmp_row.push(tmp_str.drain(..).collect());
+                table.push(tmp_row.drain(..).collect());
+            }
+            Some(c) => {
+                tmp_str.push(c);
+            }
+            None => break,
+        }
+    }
 
-	//leftover
-	tmp_row.push(tmp_str.drain(..).collect());
-	table.push(tmp_row.drain(..).collect());
+    //leftover
+    tmp_row.push(tmp_str.drain(..).collect());
+    table.push(tmp_row.drain(..).collect());
 
-	let mut out :String = table.iter().filter_map(|row|row.get(col - 1)).fold(String::new(), |mut acc, x|{
-		acc.push_str(&x);
-		acc.push('\n');
-		acc
-	});
-	out.pop();
-	let out = out;
-	
+    let mut out: String =
+        table
+            .iter()
+            .filter_map(|row| row.get(col - 1))
+            .fold(String::new(), |mut acc, x| {
+                acc.push_str(&x);
+                acc.push('\n');
+                acc
+            });
+    out.pop();
+    let out = out;
+
     write!(io::stdout(), "{}", out)?;
     io::stdout().flush()?;
     Ok(())
